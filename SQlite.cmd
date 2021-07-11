586ff8f153da6308e6bc780424e81fa197c038ec
Riccardo Marini <riccardo.marini65@gmail.com>
2021-07-11 18:40:04 +0200

    Add project files.

---
+++ SQlite.cmd
@@ -0,0 +1,3 @@
+@echo on
+set curpath=%~dp0
+%curpath%\..\..\SQLite\3.35.5\sqlite3.exe %curpath%\x64\Debug\TheWorldMap.db
---
+++ SQliteMainSchema.cmd
@@ -0,0 +1,4 @@
+@echo on
+set curpath=%~dp0
+%curpath%\..\..\SQLite\3.35.5\sqlite3.exe %curpath%\x64\Debug\TheWorldMap.db ".read D:\\TheWorld\\Client\\TheWorld_MapManager\\SQL\\CreateMainSchema.sql"
+pause

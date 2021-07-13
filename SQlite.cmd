0bebf1be04d3203b91629ede55f45db8ba9018be
Riccardo Marini <riccardo.marini65@gmail.com>
2021-07-13 15:09:34 +0200

    wip

--- SQlite.cmd
+++
@@ -1,3 +0,0 @@
-@echo on
-set curpath=%~dp0
-%curpath%\..\..\SQLite\3.35.5\sqlite3.exe %curpath%\x64\Debug\TheWorldMap.db
--- SQliteMainSchema.cmd
+++
@@ -1,4 +0,0 @@
-@echo on
-set curpath=%~dp0
-%curpath%\..\..\SQLite\3.35.5\sqlite3.exe %curpath%\x64\Debug\TheWorldMap.db ".read D:\\TheWorld\\Client\\TheWorld_MapManager\\SQL\\CreateMainSchema.sql"
-pause

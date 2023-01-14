d9e2bfd651d14d8f4ea29dc4e413cae94a658b1e
Riccardo Marini <riccardo.marini65@gmail.com>
2023-01-14 08:33:54 +0100

    wip

--- ConsoleMapManager/ConsoleMapManager.cpp
+++ ConsoleMapManager/ConsoleMapManager.cpp
@@ -10,7 +10,7 @@ using namespace TheWorld_MapManager;
 
 int main()
 {
-    int test = 3;
+    int test = 4;
 
     consoleDebugUtil debugUtil(true);
     TimerMs clock(true, true); // Timer<milliseconds, steady_clock>
@@ -55,8 +55,8 @@ int main()
 
             mapManager.LoadGISMap("D:\\TheWorld\\Prove\\untitled2.shp", true);
 
-            WorldDefiner WD1(2, 3, WDType::elevator, WDFunctionType::cosin, (float)0.5, (float)50);
-            WorldDefiner WD2(3, 4, WDType::elevator, WDFunctionType::cosin, (float)0.7, (float)50);
+            WorldDefiner WD1(2, 3, WDType::elevator, WDFunctionType::MaxEffectOnWD, (float)0.5, (float)50);
+            WorldDefiner WD2(3, 4, WDType::elevator, WDFunctionType::MaxEffectOnWD, (float)0.7, (float)50);
 
             mapManager.eraseWD(WD1);
             //mapManager.UpdateValues();
@@ -117,6 +117,47 @@ int main()
 
             mapManager.getVertices(1195425, 5465512, MapManager::anchorType::center, 25000, mesh, numPointX, numPointZ, gridStepInWU);
             mapManager.getPatches(1195425, 5465512, MapManager::anchorType::center, 25000, patches, numPatchX, numPatchZ, gridStepInWU);
+        }
+        catch (MapManagerException& e)
+        {
+            std::cout << e.exceptionName() << " caught" << std::endl;
+            std::cout << e.what() << std::endl;
+        }
+        catch (std::exception& e)
+        {
+            std::cout << "std::exception caught" << std::endl;
+            std::cout << e.what() << std::endl;
+        }
+        catch (...)
+        {
+            std::cout << "Generic exception caught" << std::endl;
+        }
+    }
+    else if (test == 4)
+    {
+        try
+        {
+            MapManager mapManager(NULL);
+            mapManager.instrument(true);
+            mapManager.consoleDebugMode(true);
+
+            WorldDefiner WD1(2, 3, WDType::elevator, WDFunctionType::MaxEffectOnWD, (float)10, (float)50);
+            WorldDefiner WD2(10, 10, WDType::elevator, WDFunctionType::MaxEffectOnWD, (float)70, (float)50);
+            WorldDefiner WD3(20, 20, WDType::depressor, WDFunctionType::MaxEffectOnWD, (float)50, (float)20);
+            WorldDefiner WD4(25, 25, WDType::flattener, WDFunctionType::MaxEffectOnWD, (float)0.8, (float)5);
+
+            bool deleted = mapManager.eraseWD(WD1);
+            deleted = mapManager.eraseWD(WD2);
+            deleted = mapManager.eraseWD(WD3);
+            deleted = mapManager.eraseWD(WD4);
+
+            __int64 rowid = mapManager.addWD(WD1);
+            rowid = mapManager.addWD(WD2);
+            rowid = mapManager.addWD(WD3);
+            rowid = mapManager.addWD(WD4);
+            mapManager.UpdateValues();
+
+            mapManager.finalizeDB();
         }
         catch (MapManagerException& e)
         {
